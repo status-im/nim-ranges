b11110240f0b1c1f1593e82e12d80b355c0d245c
Zahary Karadjov <zahary@gmail.com>
2018-12-18 01:01:46 +0200

    add pointer distance function

--- ranges/ptr_arith.nim
+++ ranges/ptr_arith.nim
@@ -3,5 +3,8 @@ proc baseAddr*[T](x: openarray[T]): pointer = cast[pointer](x)
 proc shift*(p: pointer, delta: int): pointer {.inline.} =
   cast[pointer](cast[int](p) + delta)
 
+proc distance*(a, b: pointer): int {.inline.} =
+  cast[int](b) - cast[int](a)
+
 proc shift*[T](p: ptr T, delta: int): ptr T {.inline.} =
   cast[ptr T](shift(cast[pointer](p), delta * sizeof(T)))
