0ebca43b09cca873b1e5d029d564fe3a0bf85a00
Zahary Karadjov <zahary@gmail.com>
2019-02-22 15:28:36 +0200

    doh, toOpenArray is broken in Nim 0.19.0

--- ranges/ptr_arith.nim
+++ ranges/ptr_arith.nim
@@ -9,6 +9,13 @@ proc distance*(a, b: pointer): int {.inline.} =
 proc shift*[T](p: ptr T, delta: int): ptr T {.inline.} =
   cast[ptr T](shift(cast[pointer](p), delta * sizeof(T)))
 
-template makeOpenArray*(p: pointer, T: type, len: int): auto =
-  cast[ptr UncheckedArray[T]](p).toOpenArray(0, len - 1)
+when (NimMajor,NimMinor,NimPatch) >= (0,19,9):
+  template makeOpenArray*(p: pointer, T: type, len: int): auto =
+    toOpenArray(cast[ptr UncheckedArray[T]](p), 0, len - 1)
+else:
+  template makeOpenArray*(p: pointer, T: type, len: int): auto =
+    # TODO: This is a fallback until we upgrade to 0.19.9
+    var s = newSeq[T](len)
+    copyMem(addr s[0], p, len * sizeof(T))
+    s
 
